100889cc63a4e0306f13ec6ea339f8e0576e0f5e
Clint Bowersock <52680761+cbowersock@users.noreply.github.com>
2020-08-12 23:38:13 -0400

    first pass complete, game functionality is error free

--- script.js
+++ script.js
@@ -1,7 +1,8 @@
 const boardContainer = document.getElementById('board-container');
-let playerTeam = 1;
+const resultsContainer = document.getElementById('results-container');
 
 const gameController = (() => {
+    let _playerTeam = 1;
     const buildBoard = () => {
         let _counter = 1
         gameBoard.board.forEach(() => {
@@ -13,46 +14,87 @@ const gameController = (() => {
             boardContainer.appendChild(cell);
         })
     }
-    
+
     const assignCell = (cell) => {
         cell.addEventListener('click', () => {
             if(cell.textContent != 'X' && cell.textContent != 'O') {
-                playerTeam = 1 - playerTeam;
-                gameBoard.board[cell.id - 1] = gameBoard.players[playerTeam].team;
+                switchPlayer();
+                gameBoard.board[cell.id - 1] = gameBoard.players[_playerTeam].team;
                 cell.textContent = gameBoard.board[cell.id - 1];
-                checkWinner();
+                if (!checkWinner()) {
+                    checkTie();
+                }
             }
         })
     }
 
     const checkWinner = () => {
         if (gameBoard.board[0] != '' && gameBoard.board[0] == gameBoard.board[1] && gameBoard.board[0] == gameBoard.board[2]) {
-            endGame();
+            endGame(gameBoard.board[0]);
+            return true;
         }
         else if (gameBoard.board[0] != '' && gameBoard.board[0] == gameBoard.board[3] && gameBoard.board[0] == gameBoard.board[6]) {
-            endGame();
+            endGame(gameBoard.board[0]);
+            return true;
         }
         else if (gameBoard.board[0] != '' && gameBoard.board[0] == gameBoard.board[4] && gameBoard.board[0] == gameBoard.board[8]) {
-            endGame();
+            endGame(gameBoard.board[0]);
+            return true;
         }
         else if (gameBoard.board[1] != '' && gameBoard.board[1] == gameBoard.board[4] && gameBoard.board[1] == gameBoard.board[7]) {
-            endGame();
+            endGame(gameBoard.board[1]);
+            return true;
         }
         else if (gameBoard.board[2] != '' && gameBoard.board[2] == gameBoard.board[4] && gameBoard.board[2] == gameBoard.board[6]) {
-            endGame();
+            endGame(gameBoard.board[2]);
+            return true;
         }
         else if (gameBoard.board[2] != '' && gameBoard.board[2] == gameBoard.board[5] && gameBoard.board[2] == gameBoard.board[8]) {
-            endGame();
+            endGame(gameBoard.board[2]);
+            return true;
         }
         else if (gameBoard.board[3] != '' && gameBoard.board[3] == gameBoard.board[4] && gameBoard.board[3] == gameBoard.board[5]) {
-            endGame();
+            endGame(gameBoard.board[3]);
+            return true;
         }
         else if (gameBoard.board[6] != '' && gameBoard.board[6] == gameBoard.board[7] && gameBoard.board[6] == gameBoard.board[8]) {
-            endGame();
+            endGame(gameBoard.board[6]);
+            return true;
         }
     }
-    const declareWinner = () => {
 
+    const checkTie = () => {
+        let _tieAmount = 0
+        gameBoard.board.forEach(cell => {
+            if (cell != '') {
+                _tieAmount++;
+            }
+        })
+        if (_tieAmount == 9) {
+            endGame(`nobody! It's a tie! You both lose!`);
+        } 
+    }
+
+    const resetGame = () => {
+        location.reload();
+    }
+
+    const endGame = (winner) => {
+        let button = document.createElement('button');
+        let results = document.createElement('h2');
+        button.id = 'reset-button';
+        button.textContent = 'Play Again!';
+        button.addEventListener('click', () => {
+            resetGame();
+        });
+        results.id = 'game-results';
+        results.textContent = `The Winner Is: ${winner}`;
+        resultsContainer.appendChild(results);
+        resultsContainer.appendChild(button);
+    }
+
+    const switchPlayer = () => {
+        _playerTeam = 1 - _playerTeam;
     }
 
     const startGame = () => {
@@ -61,18 +103,6 @@ const gameController = (() => {
         gameBoard.players.push(player1);
         gameBoard.players.push(player2);
         buildBoard();
-    }
-
-    const resetGame = () => {
-
-    }
-
-    const endGame = () => {
-        console.log('game over');
-    }
-
-    const switchPlayer = () => {
-
     }
 
     return {
